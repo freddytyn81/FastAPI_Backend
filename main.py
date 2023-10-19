b77812f86512185df087b4f12fcdfe2b305e2711
freddytyn81 <145816475+freddytyn81@users.noreply.github.com>
2023-10-19 12:03:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
 from typing import List, Dict
-from app.routers import producto
 from app.routers import order
 import uvicorn
 
 app = FastAPI()
 
-app.include_router(producto.router)
 app.include_router(order.router)
